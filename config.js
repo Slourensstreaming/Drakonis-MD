b8a99246511a27ae82bb8fc242148ff00087c908
Slourensstreaming <justslourens@gmail.com>
2025-07-28 09:19:10 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "drakonis~57EjAT6I#F0Ob9NEvem29hM7axXRYR7rfuTiZR_0oL7qFEmIGXg8",  // add your Session Id make sure it starts with 
+  SESSION_ID: "drakonis~szklXbpC#CJs4iZS3I5exKWLbe_fgYIVtJsJk9LnhBPBdutbQ7OM",  // add your Session Id make sure it starts with 
   OWNER_NUMBER: "50943033506", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
